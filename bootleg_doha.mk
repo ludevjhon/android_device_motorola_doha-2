415321f5702048747c73ea81101c3d737d74e569
ludevjhon <70524137+ludevjhon@users.noreply.github.com>
2020-09-30 08:56:27 -0300

    Update and rename lineage_doha.mk to bootleg_doha.mk

--- bootleg_doha.mk
+++ bootleg_doha.mk
@@ -20,7 +20,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 # Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/bootleggers/config/common_full_phone.mk)
 
 # Inherit from doha device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -28,7 +28,7 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 PRODUCT_BRAND := Motorola
 PRODUCT_DEVICE := doha
 PRODUCT_MANUFACTURER := Motorola
-PRODUCT_NAME := lineage_doha
+PRODUCT_NAME := bootleg_doha
 PRODUCT_MODEL := Moto G8 Plus
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
