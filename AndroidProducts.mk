34797bfd2c46512a8637d039ba54087f6e6b3621
ludevjhon <70524137+ludevjhon@users.noreply.github.com>
2020-09-30 08:55:07 -0300

    Create AndroidProducts.mk

---
+++ AndroidProducts.mk
@@ -0,0 +1,8 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/device.mk \
+    $(LOCAL_DIR)/bootleg_doha.mk
+
+COMMON_LUNCH_CHOICES := \
+    bootleg_doha-user \
+    bootleg_doha-userdebug \
+    _doha-eng \
